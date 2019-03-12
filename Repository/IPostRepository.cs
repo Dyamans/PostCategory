17f1174543e479997ee21070f3d6a11114d3c279
Dyamans <dgpats@gmail.com>
2019-03-12 15:30:58 +1000

    first commit

---
+++ Repository/IPostRepository.cs
@@ -0,0 +1,20 @@
+﻿using PostCategory.Models;
+using PostCategory.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostCategory.Repository
+{
+    public interface IPostRepository
+    {
+        Task<List<Category>> GetCategories();
+        Task<List<PostViewModel>> GetPosts();
+        Task<PostViewModel> GetPost(int? postId);
+        Task<int> AddPost(Post post);
+        Task<int> DeletePost(int? postId);
+        Task UpdatePost(Post post);
+
+    }
+}
